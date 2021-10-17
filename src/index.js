fe3548c6dd72ecd36b6489403d88d4c0934d38f1
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-10-18 00:17:38 +0700

    1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <p>Created by Chidpon Jaimun.</p>
+    <p>Copyright 2021.</p>
+  </div>,
+  document.getElementById("root")
+);
+
 //It should display 2 paragraph HTML elements.
 //The paragraphs should say:
 //Created by YOURNAME.
