7054b5118a530bcff4751e044114039ff288aa07
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-10-18 01:41:58 +0700

    1

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+const name = "Chidpon Jaimun";
+const year = new Date().getFullYear();
+
 ReactDom.render(
   <div>
-    <p>Created by Chidpon Jaimun.</p>
-    <p>Copyright 2021.</p>
+    <p>Created by {name}.</p>
+    <p>Copyright {year}.</p>
   </div>,
   document.getElementById("root")
 );
